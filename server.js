87e8d995f72393be948d331cccdea25ee5e01602
bupd <bupdprasanth@gmail.com>
2024-03-04 20:54:37 +0530

    add express
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const puppetScript = require("./puppetScript");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Your Puppeteer code goes here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", async (req, res) => {
+  try {
+    res.json({
+      Hello:
+        "Go to the /scrape?username=<your Email Id> to get your attendance details.",
+    });
+  } catch (err) {}
+});
+
+app.get("/scrape", async (req, res) => {
+  try {
+    const { username } = req.query;
+
+    const jsonData = await puppetScript(username);
+
+    res.json(jsonData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
