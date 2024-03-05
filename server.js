255ba54fdcfcaaab88ea6a8bdaa1bb527cf4c86f
bupd <bupdprasanth@gmail.com>
2024-03-05 19:55:44 +0530

    check basic functionality first
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require("express");
+const express = require("express")();
 const puppetScript = require("./puppetScript");
+
+let chrome = {};
+let puppeteer;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+}
 
 // Your Puppeteer code goes here
 
@@ -20,12 +28,30 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/scrape", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
   try {
-    const { username } = req.query;
+    let browser = await puppeteer.launch(options);
 
-    const jsonData = await puppetScript(username);
+    let page = await browser.newPage();
 
-    res.json(jsonData);
+    await page.goto("https://www.google.com");
+
+    res.send(await page.title());
+    // const { username } = req.query;
+    //
+    // const jsonData = await puppetScript(username);
+    //
+    // res.json(jsonData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
