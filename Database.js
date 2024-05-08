7881dd61fc916ceaf3e28447858c5b43f149680b
bupd <bupdprasanth@gmail.com>
2024-05-08 22:03:41 +0530

    init db.js

--- Database.js
+++ Database.js
@@ -1,8 +1,13 @@
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/scrape";
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("scrape");
+  var defaultobj = { name: "bupdprasanth@gmail.com", password: "dhi001" };
+  dbo.collection("customers").insertOne(defaultobj, function (err, res) {
+    if (err) throw err;
+    console.log("1 document inserted\n", res);
+    db.close();
+  });
 });
